ce99bfeecb44f5f9cfd7f94cea7d91221d346cd3
Satyam Kaushik <kaushiksatyam54@gmail.com>
2024-08-23 17:27:02 +0530

    first commit

---
+++ GFG/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public void main(String[] args) {
+        ClassA c = new ClassA();
+        ClassA d = new ClassA();
+        c.color = "RED";
+        System.out.println(c.color);
+        System.out.println(d.color);
+
+    }
+//
+}
